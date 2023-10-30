ccfaf49e594e9fba76c02d91d67d0396572a9274
ttsendo <scastao01@spy.sena.edu.co>
2023-10-30 11:23:16 -0500

    Falta integtrar la plantilla oct 30

---
+++ Ventas_VidaKids/index.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const path = require('path')//TRABAJAR CON RUTAS
+const hbs = require('hbs')// INCORPORAR MOTOR CON PLANTILLAS
+
+const app = express()
+
+const port = 8383
+
+//SERVIDOR DE CONTENIDO ESTATICO
+app.use(express.static('public'))
+
+//INGIENERIA DE LAS VISTAS: HBS
+//ASIGNAR LA UBICACION DE LOS ARCHIVOS HBS
+
+app.set('views', path.join(__dirname+'/public/views'))
+app.set('view engine', 'hbs')
+
+//CONFIGURACION DEL DIRECTORIO QUE GUARDARA LOS ARCHIVOS PARTIALS HBS
+hbs.registerPartials(__dirname+'/public/views/partials')
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        "nombre": "Diego Moreno",
+        "email": "diego.1692@hotmail.com"
+    })//REDIRECCIONAR HACIA EL ARCHIVO HBS
+})
+
+app.get('/productos', (req, res) => {
+    res.render('productos')//REDIRECCIONAR HACIA EL ARCHIVO HBS
+})
+
+app.get('/contacto', (req, res) => {
+    res.render('contacto')//REDIRECCIONAR HACIA EL ARCHIVO HBS
+})
+
+app.listen(port, () => {
+    console.log(`Listen to port: ${port}`)
+})
